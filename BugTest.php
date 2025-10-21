affd16951d41f5321d068763e7d6a81b64493e77
philzy127 <165518581+philzy127@users.noreply.github.com>
2025-10-21 15:35:48 -0400

    Update BugTest.php
    Seeing if Jules can speak to this now that it's updated and committed again.

--- BugTest.php
+++ BugTest.php
@@ -1,6 +1,32 @@
 <?php
+// Basic test of {{variable}} syntax handling
 
-{{reproduce_the_bug}}
+$vars = [
+    'Date' => '2025-10-21',
+    'Time' => '12:34:56',
+    'Name' => 'Test User'
+];
 
+$template = <<<EOT
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Template Test</title>
+</head>
+<body>
+    <h1>Variable Replacement Test</h1>
+    <p>Date: {{Date}}</p>
+    <p>Time: {{Time}}</p>
+    <p>Hello, {{Name}}!</p>
+</body>
+</html>
+EOT;
 
+// Replace placeholders
+foreach ($vars as $key => $value) {
+    $template = str_replace('{{' . $key . '}}', $value, $template);
+}
+
+echo $template;
 ?>
